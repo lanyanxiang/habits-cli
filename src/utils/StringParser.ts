4bfbb559a0207bfc8efdca63830145b1f9dcc830
Jimmy <jimmylanbcn@gmail.com>
2021-08-27 18:47:47 -0400

    Add camelToSpaceSeparated

--- src/utils/StringParser.ts
+++ src/utils/StringParser.ts
@@ -13,4 +13,20 @@ export class StringParser {
     }
     return str[0].toUpperCase() + str.slice(1).toLowerCase();
   };
+
+  /**
+   * Convert a camel cased string to a space separated string
+   * in lower case.
+   * @param str
+   */
+  static camelToSpaceSeparated = (str: string) => {
+    if (!str.length) {
+      return "";
+    }
+
+    return (
+      str[0].toLowerCase() +
+      str.slice(1).replace(/[A-Z]/g, (letter) => ` ${letter.toLowerCase()}`)
+    );
+  };
 }
