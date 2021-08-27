c0c689ceb0da303f0e03b54329a306243cad6b43
Jimmy <jimmylanbcn@gmail.com>
2021-08-27 19:03:23 -0400

    Index files

--- src/services/network/index.ts
+++ src/services/network/index.ts
@@ -1,5 +1,9 @@
 import { silentRequest } from "./silentRequest";
+import { request } from "./request";
+import { logError } from "./logError";
 
 export const network = {
+  request,
   silentRequest,
+  logError,
 };
