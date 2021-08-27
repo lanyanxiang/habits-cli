71d914989d48d14a008f6e8437950469e07d5de6
Jimmy <jimmylanbcn@gmail.com>
2021-08-27 19:12:03 -0400

    Refactor code

--- src/services/network/request.ts
+++ src/services/network/request.ts
@@ -7,6 +7,28 @@ import {
 import { spinners } from "../spinners";
 import { silentRequest } from "./silentRequest";
 import { logError } from "./logError";
+import { Ora } from "ora";
+
+const _handleSuccess = (
+  spinner: Ora,
+  requestOptions: RegularRequestOptions
+) => {
+  spinner.succeed(requestOptions.successMsg);
+  if (requestOptions.shouldClearSpinner) {
+    spinner.clear();
+  }
+};
+
+const _handleFailure = (
+  spinner: Ora,
+  requestOptions: RegularRequestOptions,
+  response: ErrorResponse
+) => {
+  spinner.fail(requestOptions.failureMsg);
+  if (!requestOptions.disableErrorLog) {
+    logError(response);
+  }
+};
 
 /**
  * Send a request using `instance` and based on `requestOptions`.
@@ -21,29 +43,14 @@ export const request = async (
   instance: AxiosInstance,
   requestOptions: RegularRequestOptions
 ): Promise<SuccessResponse | ErrorResponse> => {
-  const {
-    description,
-    successMsg,
-    failureMsg,
-    shouldClearSpinner,
-    disableErrorLog,
-    ...otherOptions
-  } = requestOptions;
+  const { description, ...otherOptions } = requestOptions;
 
   const spinner = spinners.start(description);
   const response = await silentRequest(instance, otherOptions);
 
-  // === Update spinner based on response
-  if (!response.isError) {
-    spinner.succeed(successMsg);
-    if (shouldClearSpinner) {
-      spinner.clear();
-    }
-    return response;
-  }
-  spinner.fail(failureMsg);
-  if (!disableErrorLog) {
-    logError(response);
-  }
+  response.isError
+    ? _handleFailure(spinner, requestOptions, response)
+    : _handleSuccess(spinner, requestOptions);
+
   return response;
 };
