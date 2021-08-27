9442262f2d1e324634b81942059c116e0e443bee
Jimmy <jimmylanbcn@gmail.com>
2021-08-26 23:40:20 -0400

    Remove local RequestConfig interface

--- src/services/network/request.ts
+++ src/services/network/request.ts
@@ -1,10 +1,1 @@
 import { AxiosInstance } from "axios";
-
-export interface RequestConfig {
-  /** The URI to request to. */
-  uri: string;
-  /** Instance to use. */
-  instance: AxiosInstance;
-  /** Message to display when awaiting response. */
-  loadingMsg?: string;
-}
