ae073f158b7a758a9416f3b0f188ba4f6c687684
Jimmy <jimmylanbcn@gmail.com>
2021-08-27 19:02:57 -0400

    Add logError.ts

---
+++ src/services/network/logError.ts
@@ -0,0 +1,25 @@
+import { ErrorResponse } from "../../types";
+import { StringParser } from "../../utils";
+
+export const logError = (error: ErrorResponse) => {
+  const errorStatus = error.response?.status || "local";
+  const errorTitle = StringParser.capitalize(
+    error.response?.statusText || "An error occurred"
+  );
+  const errorMessages = error.response?.data.errors?.map((error) => {
+    let msg = `[Error] ${error.message}`;
+    if (error.cause) {
+      const cause = StringParser.camelToSpaceSeparated(error.cause);
+      msg += `\n> Caused by ${cause}`;
+    }
+    return msg;
+  });
+
+  console.log(`${errorTitle} (${errorStatus})`);
+  errorMessages?.forEach((msg) => {
+    console.log(msg);
+  });
+  if (!error.response) {
+    console.log("[Error] Could not connect to the server.");
+  }
+};
