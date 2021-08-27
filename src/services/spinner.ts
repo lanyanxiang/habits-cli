e02e292925b6bb5fb98996542152d66025f047ad
Jimmy <jimmylanbcn@gmail.com>
2021-08-27 18:02:24 -0400

    Spread start options

--- src/services/spinner.ts
+++ src/services/spinner.ts
@@ -3,7 +3,7 @@ import ora, { Options as OraOptions } from "ora";
 type StartSpinnerOptions = Omit<OraOptions, "text">;
 
 const start = (text: string, options: StartSpinnerOptions) => {
-  return ora({ text }).start();
+  return ora({ text, ...options }).start();
 };
 
 export const spinner = { start };
