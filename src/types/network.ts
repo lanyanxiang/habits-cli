ea7a03e2e51b52a28ab50df51381c716ef00b285
Jimmy <jimmylanbcn@gmail.com>
2021-08-26 23:19:56 -0400

    Add RegularRequestConfig

--- src/types/network.ts
+++ src/types/network.ts
@@ -24,3 +24,16 @@ export interface DataRequestConfig extends RequestConfigBase {
 export interface DataFreeRequestConfig extends RequestConfigBase {
   method: RequestMethod.GET | RequestMethod.DELETE | RequestMethod.OPTIONS;
 }
+
+export type SilentRequestConfig = DataRequestConfig | DataFreeRequestConfig;
+
+export type RegularRequestConfig = SilentRequestConfig & {
+  /** Description of this request to be displayed while awaiting for response. */
+  description: string;
+  /** Text to display when the request is fulfilled. Or `description` if this is
+   * not specified. */
+  successMsg?: string;
+  /** Text to display when the request fails. Or `description` if this is not
+   * specified.*/
+  failureMsg?: string;
+};
