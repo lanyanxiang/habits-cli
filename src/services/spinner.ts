ef009e381e43fa39cafd204d2e96e6ef4edde06c
Jimmy <jimmylanbcn@gmail.com>
2021-08-27 18:07:50 -0400

    Add docstring

--- src/services/spinner.ts
+++ src/services/spinner.ts
@@ -2,7 +2,17 @@ import ora, { Options as OraOptions } from "ora";
 
 type StartSpinnerOptions = Omit<OraOptions, "text">;
 
-const start = (text: string, options: StartSpinnerOptions) => {
+/**
+ * Start a spinner with `text` and based on `options`.
+ * Return a spinner instance. The spinner will begin with a loading
+ * state. Update the spinner status by calling methods from
+ * the spinner instance. For example:
+ * > const spinner = spinner.start(); <br/>
+ * > spinner.succeed("success!");
+ * @param text
+ * @param options
+ */
+const start = (text: string, options?: StartSpinnerOptions) => {
   return ora({ text, ...options }).start();
 };
 
