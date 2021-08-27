8ba1bce737034fdf2a304f39cc78ede30644487e
Jimmy <jimmylanbcn@gmail.com>
2021-08-27 18:11:27 -0400

    Add request function

--- src/services/network/request.ts
+++ src/services/network/request.ts
@@ -1,1 +1,7 @@
 import { AxiosInstance } from "axios";
+import { RegularRequestOptions } from "../../types";
+
+export const request = (
+  instance: AxiosInstance,
+  requestOptions: RegularRequestOptions
+) => {};
