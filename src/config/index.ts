47f17967afde1493425a627d3f7ba5470f355ede
Jimmy <jimmylanbcn@gmail.com>
2021-08-26 21:31:15 -0400

    Add comments

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,9 @@
+/**
+ * This is a CLI application, so we don't use environment variables.
+ * While editing this file, please keep in mind that these values are
+ * completely visible to the user. Do not keep secrets here!
+ */
+
 export const config = {
   baseURL: "http://localhost:5000/api/v1",
 };
