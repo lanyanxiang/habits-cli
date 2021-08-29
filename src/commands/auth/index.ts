b26ad4577b77f90966406328ea10afcf57150732
Jimmy <jimmylanbcn@gmail.com>
2021-08-29 09:32:27 -0400

    Implement prompting logic

--- src/commands/auth/index.ts
+++ src/commands/auth/index.ts
@@ -1,5 +1,6 @@
 import { CommandGroup } from "../../models";
+import { SignInCommand } from "./signin";
 
-export const auth = new CommandGroup("auth", "authentication").withSubcommands(
-  []
-);
+export const auth = new CommandGroup("auth", "authentication").withSubcommands([
+  new SignInCommand(),
+]);
--- src/commands/auth/signin.ts
+++ src/commands/auth/signin.ts
@@ -1,6 +1,7 @@
+import inquirer from "inquirer";
+import { Option } from "commander";
 import { Command } from "../../models";
 import { network } from "../../services";
-import inquirer from "inquirer";
 import { validation } from "../../utils";
 
 interface PromptAnswers {
@@ -13,36 +14,51 @@ export class SignInCommand extends Command {
   description: string = "sign in to your habits account";
   aliases: string[] = ["login"];
 
-  private email: string = "";
-  private password: string = "";
+  private userInput: Partial<PromptAnswers> | undefined;
+
+  acceptOpts = [new Option("-e, --email <email>", "email of user")];
+
+  private _processOptions() {
+    if (this.opts.email && validation.isEmail(this.opts.email)) {
+      this.userInput = {
+        email: this.opts.email,
+      };
+    }
+  }
 
   private async _promptForCredentials() {
-    const answers = inquirer.prompt<PromptAnswers>([
-      {
-        type: "input",
-        name: "email",
-        message: "Email:",
-        validate: (
-          input: any,
-          _?: PromptAnswers
-        ): boolean | string | Promise<boolean | string> => {
-          if (validation.isEmail(input)) {
-            return true;
-          }
-          return "Please enter a valid email address.";
+    this.userInput = await inquirer.prompt<PromptAnswers>(
+      [
+        {
+          type: "input",
+          name: "email",
+          message: "Email:",
+          validate: (input, _) => {
+            if (validation.isEmail(input)) {
+              return true;
+            }
+            return "Please enter a valid email address.";
+          },
         },
-      },
-      {
-        type: "password",
-        name: "password",
-        message: "Password:",
-        mask: "*",
-      },
-    ]);
-    console.log(answers);
+        {
+          type: "password",
+          name: "password",
+          message: "Password:",
+          mask: "*",
+          validate: (input, _) => {
+            if (input?.length) {
+              return true;
+            }
+            return "Please enter a password.";
+          },
+        },
+      ],
+      this.userInput
+    );
   }
 
   async run(): Promise<void> {
+    this._processOptions();
     await this._promptForCredentials();
     return undefined;
   }
