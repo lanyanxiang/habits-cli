630ca83980547784a8cc8c97d93cf64f42a6c6ee
Jimmy <jimmylanbcn@gmail.com>
2021-08-29 19:04:20 -0400

    Add response header check

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,6 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { config } from "../config";
+import { ErrorPayload, ErrorResponse, SuccessResponse } from "../types";
 
 declare module "axios" {
   interface AxiosResponse {
@@ -26,4 +27,35 @@ mainApi.interceptors.response.use(
     error.isError = true;
     return Promise.reject(error);
   }
+);
+
+// Handle X-Refresh-Token and X-Access-Token in response header.
+// See https://lanyanxiang.github.io/node-authentication-starter/#/api/authentication.
+const _storeTokens = async <T extends SuccessResponse | ErrorResponse>(
+  _response: T
+): Promise<T> => {
+  const response:
+    | AxiosResponse<ErrorPayload>
+    | SuccessResponse
+    | undefined = _response.isError ? _response.response : _response;
+
+  if (!response?.headers) {
+    return _response;
+  }
+  if ("x-access-token" in response.headers) {
+    console.log("Received x-access-token", response.headers["x-access-token"]);
+  }
+  if ("x-refresh-token" in response.headers) {
+    console.log(
+      "Received x-refresh-token",
+      response.headers["x-refresh-token"]
+    );
+  }
+
+  return _response;
+};
+mainApi.interceptors.response.use(
+  async (response) =>
+    await _storeTokens<SuccessResponse>(response as SuccessResponse),
+  (error) => _storeTokens<ErrorResponse>(error)
 );
