64c4389e9393d0eb01442b969550ddf469798369
Jimmy <jimmylanbcn@gmail.com>
2021-08-29 19:10:50 -0400

    Add transaction command group

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { CommandGroup } from "./models";
 import { auth } from "./commands/auth";
+import { transaction } from "./commands/transaction";
 
 const start = () => {
   const rawArgs = process.argv.slice(2);
 
   // Initialize top-level command
   new CommandGroup("habits", "the habits cli")
-    .withSubcommands([auth])
+    .withSubcommands([auth, transaction])
     .init(rawArgs)
     .run();
 };
