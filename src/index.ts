a035b60038f84115db9050aa792b21e7de4b94c3
Jimmy <jimmylanbcn@gmail.com>
2021-08-28 23:48:01 -0400

    Update example

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import { LoginCommand, LogoutCommand } from "./commands/auth";
 import { CommandGroup } from "./models";
 
 const start = () => {
-  const auth = new CommandGroup("auth", "authenticate")
-    .withSubcommands([new LoginCommand(), new LogoutCommand()])
+  const auth = new CommandGroup("auth", "authenticate").withSubcommands([
+    new LoginCommand(),
+    new LogoutCommand(),
+  ]);
+  new CommandGroup("habits", "the habits cli")
+    .withSubcommands([auth])
     .init(process.argv.slice(2))
     .run();
 };
