7b12cb9a1daa8b5244846ebebe876599565b5a68
Jimmy <jimmylanbcn@gmail.com>
2021-08-29 13:02:36 -0400

    Add signup command to auth collection

--- src/commands/auth/index.ts
+++ src/commands/auth/index.ts
@@ -1,6 +1,8 @@
 import { CommandGroup } from "../../models";
 import { SignInCommand } from "./signin";
+import { SignUpCommand } from "./signup";
 
 export const auth = new CommandGroup("auth", "authentication").withSubcommands([
   new SignInCommand(),
+  new SignUpCommand(),
 ]);
