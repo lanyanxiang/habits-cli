7cf3419a81c8f9156830ed2715a9e039abbb5e43
Jimmy <jimmylanbcn@gmail.com>
2021-08-29 08:42:23 -0400

    Update indices

--- src/commands/auth/index.ts
+++ src/commands/auth/index.ts
@@ -1,36 +1,5 @@
-import { Command } from "../../models";
-import { Option } from "commander";
+import { CommandGroup } from "../../models";
 
-export class LoginCommand extends Command {
-  name: string = "login";
-  description: string = "log in to your account";
-
-  acceptOpts = [
-    new Option("-a").hideHelp(true),
-    new Option("-b"),
-    new Option("-c"),
-  ];
-
-  run(): void | Promise<void> {
-    console.log(this.args);
-    console.log(this.opts);
-  }
-}
-
-export class LogoutCommand extends Command {
-  name: string = "logout";
-  description: string = "log out of your account";
-
-  acceptOpts = [
-    new Option("-d").hideHelp(true),
-    new Option("-e"),
-    new Option("-f"),
-    new Option("-x"),
-    new Option("-y"),
-  ];
-
-  run(): void | Promise<void> {
-    console.log(this.args);
-    console.log(this.opts);
-  }
-}
+export const auth = new CommandGroup("auth", "authentication").withSubcommands(
+  []
+);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import { LoginCommand, LogoutCommand } from "./commands/auth";
 import { CommandGroup } from "./models";
+import { auth } from "./commands/auth";
 
 const start = () => {
-  const auth = new CommandGroup("auth", "authenticate").withSubcommands([
-    new LoginCommand(),
-    new LogoutCommand(),
-  ]);
+  const rawArgs = process.argv.slice(2);
+
+  // Initialize top-level command
   new CommandGroup("habits", "the habits cli")
     .withSubcommands([auth])
-    .init(process.argv.slice(2))
+    .init(rawArgs)
     .run();
 };
 
