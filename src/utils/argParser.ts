35a9aa5b6bb20f518dd02a0a77db48f7909dfec1
Jimmy <jimmylanbcn@gmail.com>
2021-08-29 19:52:54 -0400

    Add initial implementation

--- src/utils/argParser.ts
+++ src/utils/argParser.ts
@@ -1,21 +1,54 @@
 import { InvalidArgumentError } from "commander";
+import { stringParser } from "./stringParser";
+
+// TODO Add a generic validation method that is compatible with yup / joi.
 
 const _getFieldName = (field?: string) => (field ? field : "value");
 
-const handleInt = (
-  field?: string,
-  config?: Partial<{ min: number; max: number }>
+const _throwInvalidArgumentError = (message: string) => {
+  throw new InvalidArgumentError(stringParser.capitalize(message));
+};
+
+type NumberConstraint = Partial<{ min: number; max: number }>;
+
+const checkNumericValue = (
+  fieldName: string,
+  value: any,
+  config?: NumberConstraint
 ) => {
+  if (isNaN(value)) {
+    _throwInvalidArgumentError(`${fieldName} must be an integer.`);
+  }
+  if (config?.max && value > config.max) {
+    _throwInvalidArgumentError(
+      `${fieldName} cannot be more than ${config.max}.`
+    );
+  }
+  if (config?.min && value < config.min) {
+    _throwInvalidArgumentError(
+      `${fieldName} cannot be less than ${config.min}.`
+    );
+  }
+};
+
+const handleInt = (field?: string, config?: NumberConstraint) => {
+  const fieldName = _getFieldName(field);
   return (value: string, _: unknown) => {
     const parsedValue = parseInt(value);
-    if (isNaN(parsedValue)) {
-      throw new InvalidArgumentError(``);
-    }
-    if (config?.max && parsedValue > config.max) {
-      throw new InvalidArgumentError("Value must be ");
-    }
+    checkNumericValue(fieldName, parsedValue, config);
+  };
+};
+
+const handleFloat = (field?: string, config?: NumberConstraint) => {
+  const fieldName = _getFieldName(field);
+  return (value: string, _: unknown) => {
+    const parsedValue = parseInt(value);
+    checkNumericValue(fieldName, parsedValue, config);
   };
 };
 
 /** Common argument parsers for command options. */
-export const argParser = {};
+export const argParser = {
+  handleInt,
+  handleFloat,
+};
