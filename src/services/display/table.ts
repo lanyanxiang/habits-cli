cb22bbdc844affddb53889aee06cb23590b18f88
Jimmy <jimmylanbcn@gmail.com>
2021-08-30 19:29:06 -0400

    Add table service

---
+++ src/services/display/table.ts
@@ -0,0 +1,15 @@
+import Table from "cli-table3";
+
+const create = (options?: Partial<Table.TableOptions>) => {
+  return new Table({
+    style: { border: [], head: ["cyan"] },
+    wordWrap: true,
+    ...options,
+  });
+};
+
+const print = (table: Table.Table) => {
+  console.log(table.toString());
+};
+
+export const table = { create, print };
