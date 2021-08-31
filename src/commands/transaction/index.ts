226a4c597c9a7c676f18def88938f45d51fa12a2
Jimmy <jimmylanbcn@gmail.com>
2021-08-31 16:54:21 -0400

    Add create subcommand

--- src/commands/transaction/index.ts
+++ src/commands/transaction/index.ts
@@ -1,9 +1,10 @@
 import { CommandGroup } from "../../models";
 import { ListCommand } from "./list";
+import { CreateCommand } from "./create";
 
 export const transaction = new CommandGroup(
   "transaction",
   "manage changes in points"
 )
   .withAliases(["tran"])
-  .withSubcommands([new ListCommand()]);
+  .withSubcommands([new ListCommand(), new CreateCommand()]);
