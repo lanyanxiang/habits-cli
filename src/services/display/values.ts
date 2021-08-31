5e7bf21e248fdf6d2552a4db230b7553e0423532
Jimmy <jimmylanbcn@gmail.com>
2021-08-31 03:37:21 -0400

    Add formatPoints

--- src/services/display/values.ts
+++ src/services/display/values.ts
@@ -2,4 +2,18 @@
  * Methods for value display.
  */
 
-export const values = {};
+/**
+ * Get string for `points` that can be output by `console.log` or
+ * equivalent.
+ * @param points Points to display.
+ */
+const formatPoints = (points: number) => {
+  const pointsLocalStr = points.toLocaleString(undefined, {
+    maximumFractionDigits: 2,
+  });
+  return points < 0 ? `(${pointsLocalStr})` : pointsLocalStr;
+};
+
+export const values = {
+  formatPoints,
+};
