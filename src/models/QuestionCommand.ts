73bf27a4b83051fb3543f900643837fb621168ab
Jimmy <jimmylanbcn@gmail.com>
2021-08-31 06:21:44 -0400

    Add QuestionCommand.ts

---
+++ src/models/QuestionCommand.ts
@@ -0,0 +1,9 @@
+import {Command} from "./Command";
+
+/**
+ * A blueprint for command that simply prompts for user inputs once, then move on
+ * to other tasks.
+ */
+export abstract class QuestionCommand extends Command {
+
+}
