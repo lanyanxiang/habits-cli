2c482c79225f6949781ec1b3c8e11bc2280a846b
Jimmy <jimmylanbcn@gmail.com>
2021-08-31 07:10:07 -0400

    Add sign out command

--- src/commands/auth/index.ts
+++ src/commands/auth/index.ts
@@ -1,8 +1,10 @@
 import { CommandGroup } from "../../models";
 import { SignInCommand } from "./signin";
 import { SignUpCommand } from "./signup";
+import { SignOutCommand } from "./signout";
 
 export const auth = new CommandGroup("auth", "authentication").withSubcommands([
   new SignInCommand(),
   new SignUpCommand(),
+  new SignOutCommand(),
 ]);
