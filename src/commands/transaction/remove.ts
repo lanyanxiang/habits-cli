546e3fad5a09dda00ecf0d5764ed8c42d8777a0e
Jimmy <jimmylanbcn@gmail.com>
2021-08-31 18:59:47 -0400

    Add RemoveCommand class

--- src/commands/transaction/remove.ts
+++ src/commands/transaction/remove.ts
@@ -1,5 +1,7 @@
 import { QuestionCollection } from "inquirer";
 import { requiredValidator } from "../../utils";
+import { QuestionCommand } from "../../models";
+import { Argument } from "commander";
 
 interface PromptAnswers {
   transactionId: string;
@@ -14,3 +16,21 @@ const promptQuestions: QuestionCollection<PromptAnswers> = [
     validate: requiredValidator,
   },
 ];
+
+export class RemoveCommand extends QuestionCommand<PromptAnswers> {
+  name = "remove";
+  description = "remove a transaction";
+  aliases = ["delete", "rm"];
+  acceptArgs = [
+    new Argument("transactionId", "object id of the transaction to be removed"),
+  ];
+  protected promptQuestions = promptQuestions;
+
+  run(): void | Promise<void> {
+    return undefined;
+  }
+
+  protected mapArgumentsToInputs(): void | Promise<void> {
+    return super.mapArgumentsToInputs();
+  }
+}
