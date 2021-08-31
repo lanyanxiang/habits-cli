8572bd9d6b24282f643a657ccd1b9afed7f853a0
Jimmy <jimmylanbcn@gmail.com>
2021-08-31 16:59:42 -0400

    Add promptQuestions

--- src/commands/transaction/create.ts
+++ src/commands/transaction/create.ts
@@ -1,16 +1,42 @@
 import { QuestionCommand } from "../../models";
+import { QuestionCollection } from "inquirer";
+import { requiredValidator } from "../../utils";
 
 interface PromptAnswers {
   title: string;
   pointsChange: string;
 }
 
+const promptQuestions: QuestionCollection<PromptAnswers> = [
+  {
+    type: "input",
+    name: "title",
+    message: "Transaction title:",
+    validate: requiredValidator,
+  },
+  {
+    type: "number",
+    name: "pointsChange",
+    message: "Change in points:",
+    validate: (input: any) => {
+      if (Number(input) != 0) {
+        return true;
+      }
+      return (
+        "Change in points cannot be 0. Enter a positive " +
+        "number to add points, and a negative number to reduce " +
+        "points."
+      );
+    },
+  },
+];
+
 export class CreateCommand extends QuestionCommand<PromptAnswers> {
   name = "create";
   description = "create a new transaction";
   aliases = ["add"];
 
-  protected promptQuestions = [];
+  protected promptQuestions = promptQuestions;
 
   run(): void | Promise<void> {
     return undefined;
