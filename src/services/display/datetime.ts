db6a9442d4b0c9de42f43dd6a867d80ef8192fc4
Jimmy <jimmylanbcn@gmail.com>
2021-08-31 03:16:31 -0400

    Add formatRelative

---
+++ src/services/display/datetime.ts
@@ -0,0 +1,19 @@
+import dateFns from "date-fns";
+
+const format = (date: Date, format: string) => {
+  return dateFns.format(date, format);
+};
+
+const formatDate = (date: Date, showYear?: boolean) => {
+  const formatStr = showYear ? "MMM D, yyyy" : "MMM D";
+  return dateFns.format(date, formatStr);
+};
+
+const formatTime = (date: Date, showSeconds?: boolean) => {
+  const formatStr = showSeconds ? "HH:mm:ss" : "HH:mm";
+  return dateFns.format(date, formatStr);
+};
+
+const formatRelative = (date: Date, currentDate: Date = new Date()) => {
+  return dateFns.formatDistance(currentDate, date, { addSuffix: true });
+};
