eb1c965a3e0da66ca8bd225f3bd6198d02266e75
Jimmy <jimmylanbcn@gmail.com>
2021-08-31 17:32:28 -0400

    Add promptQuestions

--- src/commands/transaction/update.ts
+++ src/commands/transaction/update.ts
@@ -1,9 +1,30 @@
 import { QuestionCommand } from "../../models";
+import { QuestionCollection } from "inquirer";
+import { pointsChangeValidator, requiredValidator } from "../../utils";
 
 interface PromptAnswers {
   transactionId: string;
   title?: string;
   pointsChange?: number;
 }
+
+const promptQuestions: QuestionCollection<PromptAnswers> = [
+  {
+    type: "input",
+    name: "transactionId",
+    // TODO Add validation for object IDs
+    validate: requiredValidator,
+  },
+  {
+    type: "input",
+    name: "title",
+    validate: requiredValidator,
+  },
+  {
+    type: "number",
+    name: "pointsChange",
+    validate: pointsChangeValidator,
+  },
+];
 
 export class UpdateCommand extends QuestionCommand<any> {}
