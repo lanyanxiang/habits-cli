6fe9f10a8e6a51153f6afa0365f18091ee0abdc9
Jimmy <jimmylanbcn@gmail.com>
2021-08-31 20:56:08 -0400

    Add UpdateCommand

--- src/commands/property/index.ts
+++ src/commands/property/index.ts
@@ -1,6 +1,7 @@
 import { CommandGroup } from "../../models";
 import { ShowCommand } from "./show";
+import { UpdateCommand } from "./update";
 
 export const property = new CommandGroup("property", "manage your properties")
   .withAliases(["prop"])
-  .withSubcommands([new ShowCommand()]);
+  .withSubcommands([new ShowCommand(), new UpdateCommand()]);
---
+++ src/commands/property/update.ts
@@ -0,0 +1,58 @@
+import { QuestionCommand } from "../../models";
+import { QuestionCollection } from "inquirer";
+import { Option } from "commander";
+import { argParser } from "../../utils";
+import { ErrorResponse, RequestMethod, SuccessResponse } from "../../types";
+import { mainApi, network } from "../../services";
+
+interface PromptAnswers {
+  points: number;
+}
+
+const promptQuestions: QuestionCollection<PromptAnswers> = [
+  {
+    type: "number",
+    name: "points",
+    message: "New total points:",
+  },
+];
+
+export class UpdateCommand extends QuestionCommand<PromptAnswers> {
+  name = "update";
+  description = "update your properties";
+  aliases = ["change"];
+
+  protected promptQuestions = promptQuestions;
+
+  acceptOpts = [
+    new Option(
+      "-p, --points <points>",
+      "new points value to be stored"
+    ).argParser(argParser.handleInt("points")),
+  ];
+
+  protected mapOptionsToInputs(): void | Promise<void> {
+    const userInput: Partial<PromptAnswers> = this.userInput || {};
+
+    if (this.opts.points) {
+      userInput.points = this.opts.points;
+    }
+
+    this.userInput = userInput;
+  }
+
+  private async _sendRequest(): Promise<SuccessResponse | ErrorResponse> {
+    return await network.request(mainApi, {
+      uri: "/properties",
+      method: RequestMethod.PATCH,
+      data: this.userInput,
+      description: "Update properties",
+    });
+  }
+
+  async run(): Promise<void> {
+    this.mapOptionsToInputs();
+    await this.promptForInputs();
+    await this._sendRequest();
+  }
+}
