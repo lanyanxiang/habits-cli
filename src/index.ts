331ac7659a261b07172fd950c204ed146c81c6c4
Jimmy <jimmylanbcn@gmail.com>
2021-09-01 23:05:50 -0400

    Add global command version display

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import { CommandGroup } from "./models";
 import { auth, property, transaction } from "./commands";
 
+// @ts-ignore
+import { version } from "../package.json";
+
 const start = () => {
   const rawArgs = process.argv.slice(2);
 
   // Initialize top-level command
-  new CommandGroup("habits", "the habits cli")
+  const habits = new CommandGroup("habits", "the habits cli")
     .withSubcommands([auth, transaction, property])
-    .init(rawArgs)
-    .run();
+    .init(rawArgs);
+  habits.version = version;
+  habits.run();
 };
 
 start();
