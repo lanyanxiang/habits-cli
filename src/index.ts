3c0dc7376c7329cf45d3420f8b3fb8b76098b396
Jimmy <jimmylanbcn@gmail.com>
2021-09-01 23:08:25 -0400

    Fix bugs

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,13 @@ const start = () => {
   const rawArgs = process.argv.slice(2);
 
   // Initialize top-level command
-  const habits = new CommandGroup("habits", "the habits cli")
-    .withSubcommands([auth, transaction, property])
-    .init(rawArgs);
+  const habits = new CommandGroup("habits", "the habits cli").withSubcommands([
+    auth,
+    transaction,
+    property,
+  ]);
   habits.version = version;
-  habits.run();
+  habits.init(rawArgs).run();
 };
 
 start();
