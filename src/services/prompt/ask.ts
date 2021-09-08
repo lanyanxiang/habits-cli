1d7c787c5c61aa7800242963a31c57d8dbd667d4
Jimmy <jimmylanbcn@gmail.com>
2021-09-08 19:41:09 -0400

    Update import syntax

--- src/services/prompt/ask.ts
+++ src/services/prompt/ask.ts
@@ -1,8 +1,7 @@
-import { QuestionCollection } from "inquirer";
 import { inquirer } from "../inquirer";
 
 export const ask = <T>(
-  questions: QuestionCollection<T>,
+  questions: inquirer.QuestionCollection<T>,
   initialAnswers?: Partial<T>
 ) => {
   return inquirer.prompt<T>(questions, initialAnswers);
