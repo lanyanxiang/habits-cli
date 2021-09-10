ea08c1c36c48ea3cad9ac152eb787b35c21fdc6c
Jimmy <jimmylanbcn@gmail.com>
2021-09-10 18:02:37 -0400

    Add runner.ts

---
+++ src/services/runner.ts
@@ -0,0 +1,11 @@
+import { Command } from "../models";
+
+const start = (command: Command, argv: string[]) => {};
+
+/**
+ * Command runner to initialize and run a command based on arguments
+ * (and options) passed to the command.
+ */
+export const runner = {
+  start,
+};
