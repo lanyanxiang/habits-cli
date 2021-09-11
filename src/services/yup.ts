6e8b6f891989b38998d013f61200930a9c6ed312
Jimmy <jimmylanbcn@gmail.com>
2021-09-10 21:45:10 -0400

    Add pointsChange method

--- src/services/yup.ts
+++ src/services/yup.ts
@@ -1,9 +1,20 @@
-import yup, { setLocale } from "yup";
+import yup, { NumberSchema, setLocale } from "yup";
 
 setLocale({
   string: {
     email: "'${value} is not a valid email address.",
   },
 });
+
+function pointsChange(this: NumberSchema) {
+  return this.notOneOf(
+    [0],
+    "Change in points cannot be 0. Enter a positive " +
+      "number to add points, and a negative number to reduce " +
+      "points."
+  );
+}
+
+yup.addMethod(yup.number, "pointsChange", pointsChange);
 
 export { yup };
