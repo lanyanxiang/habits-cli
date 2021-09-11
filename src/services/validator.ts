d298549f3bf287dc013dbeaa4e5329a5589db78a
Jimmy <jimmylanbcn@gmail.com>
2021-09-11 14:46:51 -0400

    Add constructArgParser

--- src/services/validator.ts
+++ src/services/validator.ts
@@ -1,11 +1,12 @@
 import { BaseSchema } from "yup";
+import { InvalidArgumentError } from "commander";
 
 /**
  * Construct a validator function that takes a value and returns `true`
  * on validation successes, and a string stating the error on validation
  * failures.
  * @param schema Validation schema for this value. Construct this schema
- * using `vschema` exported from the `service` folder.
+ * using `vschema` exported from the `services` folder.
  */
 const construct = (schema: BaseSchema) => {
   return async (value: any) => {
@@ -15,6 +16,24 @@ const construct = (schema: BaseSchema) => {
     } catch (error: any) {
       return error.message;
     }
+  };
+};
+
+/**
+ * Construct an arg parser that takes a value and casts it to the corresponding
+ * type defined in `schema`. If the value cannot be parsed, throw an error that
+ * is compatible with the `Command` class.
+ * @param schema Validation schema for this value. Construct this schema
+ * using `vschema` exported from the `services` folder.
+ */
+const constructArgParser = (schema: BaseSchema) => {
+  return async (value: any) => {
+    try {
+      await schema.validate(value);
+    } catch (error: any) {
+      throw new InvalidArgumentError(error.message);
+    }
+    return schema.cast(value);
   };
 };
 
