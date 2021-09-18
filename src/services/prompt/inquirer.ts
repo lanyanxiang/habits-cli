c82552d9e3b6a42a4d72184c273f5b1523e7b78b
Jimmy <jimmylanbcn@gmail.com>
2021-09-18 09:45:24 -0400

    Register type definitions for autocomplete prompt

--- src/services/prompt/inquirer.ts
+++ src/services/prompt/inquirer.ts
@@ -1,5 +1,13 @@
-import inquirer from "inquirer";
-import autocompletePrompt from "inquirer-autocomplete-prompt";
+import inquirer, { Answers } from "inquirer";
+import autocompletePrompt, {
+  AutocompleteQuestionOptions,
+} from "inquirer-autocomplete-prompt";
+
+declare module "inquirer" {
+  interface QuestionMap<T extends Answers = Answers> {
+    autocomplete: AutocompleteQuestionOptions<T>;
+  }
+}
 
 inquirer.registerPrompt("autocomplete", autocompletePrompt);
 
