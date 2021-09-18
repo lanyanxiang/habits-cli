9e401f441d25aa2d2021e5783590577ddbd8d2e2
Jimmy <jimmylanbcn@gmail.com>
2021-09-18 10:50:40 -0400

    Add fetchProperties method

--- src/services/prompt/selectProperty.ts
+++ src/services/prompt/selectProperty.ts
@@ -1,3 +1,18 @@
-import { UserProperty } from "../../types";
+import { RequestMethod, UserProperty } from "../../types";
+import { network } from "../network";
+import { mainApi } from "../axios";
 
-export const selectProperty = (): Partial<UserProperty> => {};
+const fetchProperties = async () => {
+  return await network.request(mainApi, {
+    uri: "/properties",
+    method: RequestMethod.GET,
+    description: "Fetch your properties",
+    shouldClearSpinner: true,
+  });
+};
+
+/**
+ * Prompt the user to select one property from the properties that they have.
+ * Return the user-selected property.
+ */
+export const selectProperty = async (): Partial<UserProperty> => {};
