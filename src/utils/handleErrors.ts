566e4ea488ae26e951b918ef8e1c08348c71ec96
Jimmy <jimmylanbcn@gmail.com>
2021-09-19 11:14:43 -0400

    Add handleSingleError helper function

--- src/utils/handleErrors.ts
+++ src/utils/handleErrors.ts
@@ -1,1 +1,10 @@
+import { RuntimeError } from "../models";
+import chalk from "chalk";
+
+const handleSingleError = (error: unknown) => {
+  if (error instanceof RuntimeError) {
+    console.error(chalk.red(error.serialize()));
+  }
+};
+
 export const handleErrors = (errors: unknown) => {};
