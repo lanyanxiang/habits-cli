ef85f5db516ad26438265662564742d63f63ffe4
Jimmy <jimmylanbcn@gmail.com>
2021-09-19 11:23:54 -0400

    Implement handleSingleError

--- src/utils/handleErrors.ts
+++ src/utils/handleErrors.ts
@@ -1,10 +1,22 @@
+import { repository } from "../../package.json";
+
 import { RuntimeError } from "../models";
 import chalk from "chalk";
 
-const handleSingleError = (error: unknown) => {
+const handleSingleError = (error: any) => {
   if (error instanceof RuntimeError) {
-    console.error(chalk.red(error.serialize()));
+    return console.error(chalk.red(error.serialize()));
   }
+  if (error.message) {
+    const _err = new RuntimeError(error.message);
+    return console.error(chalk.red(_err.serialize()));
+  }
+  console.error(error);
+  return console.error(
+    chalk.red("Sorry, something went wrong. Please report this issue to ") +
+      chalk.underline(chalk.bold(repository)) +
+      chalk.red(".")
+  );
 };
 
-export const handleErrors = (errors: unknown) => {};
+export const handleErrors = (errors: any) => {};
