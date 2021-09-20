b9a7dadabe3f3c95208afee05e393e3e3fe7331b
Jimmy <jimmylanbcn@gmail.com>
2021-09-20 18:37:49 -0400

    Remove error log color

--- src/utils/handleErrors.ts
+++ src/utils/handleErrors.ts
@@ -5,17 +5,17 @@ import chalk from "chalk";
 
 const handleSingleError = (error: any) => {
   if (error instanceof RuntimeError) {
-    return console.error(chalk.red(error.serialize()));
+    return console.error(error.serialize());
   }
   if (error.message) {
     const _err = new RuntimeError(error.message);
-    return console.error(chalk.red(_err.serialize()));
+    return console.error(_err.serialize());
   }
   console.error(error);
   return console.error(
-    chalk.red("Sorry, something went wrong. Please report this issue to ") +
+    "Sorry, something went wrong. Please report this issue to " +
       chalk.underline(chalk.bold(repository)) +
-      chalk.red(".")
+      "."
   );
 };
 
