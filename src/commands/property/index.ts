ba4fcec03943058a26f68405f8c9ab0d27435ef7
Jimmy <jimmylanbcn@gmail.com>
2021-09-24 18:38:30 -0400

    Index property create command

--- src/commands/property/index.ts
+++ src/commands/property/index.ts
@@ -1,7 +1,12 @@
 import { CommandGroup } from "../../models";
 import { UpdateCommand } from "./update";
 import { ListCommand } from "./list";
+import { CreateCommand } from "./create";
 
 export const property = new CommandGroup("property", "manage your properties")
   .withAliases(["prop"])
-  .withSubcommands([new ListCommand(), new UpdateCommand()]);
+  .withSubcommands([
+    new ListCommand(),
+    new UpdateCommand(),
+    new CreateCommand(),
+  ]);
