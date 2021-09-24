3336a4cc7cd53f4c98f7a0d0a07153d1ba45336c
Jimmy <jimmylanbcn@gmail.com>
2021-09-24 18:25:31 -0400

    Add answers structure for property create command

--- src/commands/property/create.ts
+++ src/commands/property/create.ts
@@ -1,5 +1,11 @@
 import { QuestionCommand } from "../../models";
 
+interface PromptAnswers {
+  name: string;
+  description?: string;
+  amountInStock?: number;
+}
+
 export class CreateCommand extends QuestionCommand {
   name = "create";
   description = "create a new property";
