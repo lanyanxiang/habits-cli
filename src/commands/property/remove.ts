97e45a15b70eb6559f166cdfd215d3fadc8332c1
Jimmy <jimmylanbcn@gmail.com>
2021-09-24 19:45:50 -0400

    Add acceptArgs class property

--- src/commands/property/remove.ts
+++ src/commands/property/remove.ts
@@ -1,9 +1,16 @@
 import { QuestionCommand } from "../../models";
+import { Argument } from "commander";
 
 export class RemoveCommand extends QuestionCommand {
   name = "remove";
-  description = "remove a property";
+  description = "remove one or more properties";
   aliases = ["delete", "rm"];
+  acceptArgs = [
+    new Argument(
+      "<propertyId...>",
+      "object IDs of the property to be removed"
+    ).argOptional(),
+  ];
 
   protected run(): void | Promise<void> {
     return undefined;
