8648285e54c028f29b8f3a1a081abfcbd888b075
Jimmy <jimmylanbcn@gmail.com>
2021-09-24 20:25:49 -0400

    Override mapArgumentsToInputs method

--- src/commands/property/remove.ts
+++ src/commands/property/remove.ts
@@ -1,6 +1,11 @@
 import { QuestionCommand } from "../../models";
 import { Argument } from "commander";
 
+interface PromptAnswers {
+  /** Property IDs to perform delete on. */
+  propertyIds: string[];
+}
+
 export class RemoveCommand extends QuestionCommand {
   name = "remove";
   description = "remove one or more properties";
@@ -11,6 +16,16 @@ export class RemoveCommand extends QuestionCommand {
       "object IDs of the property to be removed"
     ).argOptional(),
   ];
+
+  protected mapArgumentsToInputs(): void | Promise<void> {
+    const userInput: Partial<PromptAnswers> = this.userInput || {};
+
+    if (this.args.length) {
+      userInput.propertyIds = this.args;
+    }
+
+    this.userInput = userInput;
+  }
 
   protected run(): void | Promise<void> {
     return undefined;
