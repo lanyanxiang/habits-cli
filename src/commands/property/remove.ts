52f13fe9f47756cf2cd610481bd4b269fd2f31e2
Jimmy <jimmylanbcn@gmail.com>
2021-09-24 20:34:29 -0400

    Add printInstructions

--- src/commands/property/remove.ts
+++ src/commands/property/remove.ts
@@ -1,10 +1,27 @@
 import { QuestionCommand } from "../../models";
 import { Argument } from "commander";
+import chalk from "chalk";
 
 interface PromptAnswers {
   /** Property IDs to perform delete on. */
   propertyIds: string[];
 }
+
+const printInstructions = () => {
+  console.log(
+    "Please enter the IDs of the properties that you would like to remove.\n"
+  );
+  console.log(chalk.italic(chalk.bold("Instructions: ")));
+  console.log(`- Enter ${chalk.cyan(chalk.bold("one ID"))} per row. `);
+  console.log(
+    `- Press ${chalk.cyan(
+      chalk.bold("enter")
+    )} without entering anything to finish.`
+  );
+  console.log(
+    `- Enter ${chalk.cyan(chalk.bold("at least one"))} property ID to remove.`
+  );
+};
 
 export class RemoveCommand extends QuestionCommand {
   name = "remove";
