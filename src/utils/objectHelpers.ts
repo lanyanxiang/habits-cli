1ed9fbf652e2d8aab301a59e6e146d5a35a1166b
Jimmy <jimmylanbcn@gmail.com>
2021-10-13 22:37:59 -0400

    Add docstring for selectKeys helper

--- src/utils/objectHelpers.ts
+++ src/utils/objectHelpers.ts
@@ -1,3 +1,10 @@
+/**
+ * Return a **new** object containing only `keys` from `obj`.
+ * That is, the **new** object returned will be a **subset** of the
+ * original object `obj`.
+ * @param obj Object to parse.
+ * @param keys Keys in `obj` to include in the newly returned object.
+ */
 export const selectKeys = <T, K extends keyof T>(
   obj: T,
   ...keys: K[]
