92a3677da9adf94f70dc67dc7fb3746a0eb5d681
Jimmy <jimmylanbcn@gmail.com>
2021-10-21 19:38:50 -0400

    Update property id option flag

--- src/commands/transaction/list.ts
+++ src/commands/transaction/list.ts
@@ -73,7 +73,7 @@ export class ListCommand extends QuestionCommand<PromptAnswers> {
       "number of transactions to display"
     ).argParser(validation.argParser(vschema.number().pageLimit())),
     new Option(
-      "-p, --property-id [propertyId]",
+      "-p, --property [propertyId]",
       "ID of the property involved in this transaction - " +
         "if not provided, a select prompt will be shown."
     ).argParser(validation.argParser(vschema.string().objectId())),
