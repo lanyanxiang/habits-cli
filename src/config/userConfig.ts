70dfc956dfc4608fd450a0c1f4ce1e04408ee0fe
Jimmy <jimmylanbcn@gmail.com>
2021-11-08 08:03:51 -0500

    Implement user config methods

--- src/config/userConfig.ts
+++ src/config/userConfig.ts
@@ -1,6 +1,34 @@
 import { Endpoints } from "../enums";
+import { storage } from "../services";
 
-export interface UserConfig {
+export interface UserConfig extends Record<string, any> {
   /** Endpoint to send requests to. */
   endpoint: Endpoints;
 }
+
+/**
+ * A private memoization of previously loaded configurations.
+ */
+const configMemo: { [p: string]: any } = {};
+
+const get = (key: keyof UserConfig & string) => {
+  if (!(key in configMemo)) {
+    /* Note that when a user-defined config is not found on disk,
+     * a `null` value will be inserted into config memoization and
+     * further lookups will be prevented by the"key in" syntax. */
+    configMemo[key] = storage.local.get(key);
+  }
+  return configMemo[key];
+};
+
+const set = <T extends keyof UserConfig & string>(
+  key: T,
+  value: UserConfig[T]
+) => {
+  storage.local.set(key, value);
+};
+
+export const userConfig = {
+  get,
+  set,
+};
