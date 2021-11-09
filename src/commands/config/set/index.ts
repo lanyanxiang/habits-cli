e92364fa9d0dd7b981b8f991dfe933dd648ab392
Jimmy <jimmylanbcn@gmail.com>
2021-11-08 19:53:59 -0500

    Group set endpoint command

--- src/commands/config/set/index.ts
+++ src/commands/config/set/index.ts
@@ -1,6 +1,7 @@
 import { CommandGroup } from "../../../models";
+import { SetEndpointCommand } from "./endpoint";
 
 export const set = new CommandGroup(
   "set",
   "set value for a configuration field"
-).withSubcommands([]);
+).withSubcommands([new SetEndpointCommand()]);
