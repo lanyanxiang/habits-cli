4b5f258aa2d8b8f6a41e83b466782c05d3188511
Jimmy <jimmylanbcn@gmail.com>
2021-11-08 19:48:52 -0500

    Add SetEndpointCommand class

---
+++ src/commands/config/set/endpoint.ts
@@ -0,0 +1,50 @@
+import { QuestionCommand } from "../../../models";
+import { Argument } from "commander";
+import { validation, vschema } from "../../../services";
+import { matchSorter } from "match-sorter";
+import { Endpoints } from "../../../enums";
+
+interface PromptAnswers {
+  endpointName: string;
+}
+
+export class SetEndpointCommand extends QuestionCommand<PromptAnswers> {
+  name = "endpoint";
+  description = "set the endpoint that the habits client requests to";
+
+  acceptArgs = [
+    new Argument("name", "name of the endpoint to use")
+      .argOptional()
+      .argParser(
+        validation.argParser(vschema.string().oneOf(Object.keys(Endpoints)))
+      ),
+  ];
+
+  protected mapArgumentsToInputs(): void | Promise<void> {
+    if (this.args.length > 0) {
+      this.userInput.endpointName = this.args[0];
+    }
+  }
+
+  private async _askForInput(): Promise<void> {
+    const endpointsToSelect = Object.keys(Endpoints);
+
+    await this.promptForInputs([
+      {
+        name: "endpointName",
+        message: "Which endpoint would you like to use?",
+        type: "autocomplete",
+        source: (_, input) => {
+          if (!input) {
+            return endpointsToSelect;
+          }
+          return matchSorter(endpointsToSelect, input);
+        },
+      },
+    ]);
+  }
+
+  protected async run(): Promise<void> {
+    await this._askForInput();
+  }
+}
